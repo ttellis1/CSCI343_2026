8894bf0d134d90620758c0d75f52db9b55f02cfd
ttellis1 <ttellis@coastal.edu>
2026-09-22 18:31:53 -0400

    Delete Week 5/RestaurantMenuApp directory

--- Week 5/RestaurantMenuApp/constants/Color.js
+++
@@ -1,9 +0,0 @@
-export default {
-  primary: '#D35400',
-  secondary: '#F39C12',
-  background: '#FDFEFE',
-  cardBackground: '#FFFFFF',
-  textDark: '#2C3E50',
-  textLight: '#7F8C8D',
-  white: '#FFFFFF',
-};
